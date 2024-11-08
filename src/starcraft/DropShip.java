67842a09d42d1661b7c6294fc2c0bf12ceb4db8e
ohserin <dhtpfls321@gmail.com>
2024-11-08 16:23:30 +0900

    Update DropShip protected and flying()

--- src/starcraft/DropShip.java
+++ src/starcraft/DropShip.java
@@ -1,5 +1,13 @@
 package starcraft;
 
-public class DropShip {
+public class DropShip extends AirUnit implements Repairable {
+	protected DropShip(String name, int hp) {
+		super("DropShip", 120);
+	}
+
+	@Override
+	void flying() {
+		System.out.println("난 날수있어요");
+	}
 
 }
