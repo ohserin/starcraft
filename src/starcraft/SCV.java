e176361b4fcdbbe1e7178785aa73157fa822cdf9
ohserin <dhtpfls321@gmail.com>
2024-11-08 16:19:22 +0900

    Update SCV class protected and repair : method

--- src/starcraft/SCV.java
+++ src/starcraft/SCV.java
@@ -1,5 +1,26 @@
 package starcraft;
 
-public class SCV {
+public class SCV extends GroundUnit implements Repairable {
 
+	protected SCV(String name, int hp) {
+		super("scv", 90);
+	}
+
+	void repair(Repairable unit) {
+		if (unit instanceof Unit) {
+			Unit target = (Unit) unit;
+
+			while (target.hp < target.MAX_HP) {
+				target.hp++;
+				String msg = String.format("%s 수리중 [%d/%d]", target.name, target.hp, target.MAX_HP);
+				System.out.println(msg);
+
+				try {
+					Thread.sleep(300);
+				} catch (Exception e) {
+				}
+			}
+			System.out.println("수리완료 >>>");
+		}
+	}
 }
