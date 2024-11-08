105bef13b249290b24e63739867d242e7853c851
ohserin <dhtpfls321@gmail.com>
2024-11-08 15:23:22 +0900

    Add Unit Class

--- src/starcraft/Unit.java
+++ src/starcraft/Unit.java
@@ -2,5 +2,16 @@ package starcraft;
 
 abstract public class Unit {
 
+	final int MAX_HP;
+	int hp;
+	String name;
+
+	Unit(String name, int hp) {
+		MAX_HP = hp;
+		this.hp = hp;
+		this.name = name;
+
+		System.out.println(name + "의 탄생..");
+	}
 
 }
